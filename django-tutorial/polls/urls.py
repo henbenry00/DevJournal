2e10b9f5aaf3376a019a587b3fe6517ceae71b6e
henbenry00 <benhenry0519@outlook.com>
2025-11-26 13:21:00 -0600

    Adding polls urls module

--- django-tutorial/polls/urls.py
+++ django-tutorial/polls/urls.py
@@ -5,3 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
