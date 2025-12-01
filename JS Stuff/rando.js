dd30643061bbde2c89fa36bac3ea9e0ad4a3d770
Ben Henry <158855745+henbenry00@users.noreply.github.com>
2025-12-01 12:18:24 -0600

    Made some new variables
    12/1/25; just playing around with this and testing the workflow.

--- JS Stuff/rando.js
+++ JS Stuff/rando.js
@@ -2,7 +2,13 @@ console.log("Hello, World!");
 
 // Writing some more stuff here later.
 // Vairable definition and use with print statement.
-let name = "Ben"
-let age = "25"
+let name = 'Ben';
+let age = '25';
 
-console.log("Hello, my name is", name, "and I am", age, "Years old")
+console.log("Hello, my name is", name, "and I am", age, "Years old");
+// Making a new line/
+let action = 'fooled';
+
+let newname = 'John';
+
+console.log(newname, action, "me");
